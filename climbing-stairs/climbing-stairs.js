181e936304028a007fd1f667bc48b802be70e6fd
Kim Yeseul <75363223+yesl-kim@users.noreply.github.com>
2021-08-10 14:45:30 +0900

    Time: 72 ms (64.84%), Space: 38.9 MB (9.91%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 0 || n === 1) { return 1 }
+    let dp = []
+    dp[1] = 1
+    dp[2] = 2
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp [i-2]
+    }
+    return dp[n]
+};
