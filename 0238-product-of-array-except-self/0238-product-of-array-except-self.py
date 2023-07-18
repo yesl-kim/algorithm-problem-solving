8a413b0f035729507a53519bb9d118bfb88de259
Yeseul, Kim <yesl00k624@gmail.com>
2023-07-18 11:28:04 +0900

    Time: 241 ms (57.73%), Space: 25.6 MB (16.54%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,9 +3,7 @@ from operator import mul
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre,suf=list(accumulate(nums,mul)), list(accumulate(nums[::-1],mul))[::-1]
-        for i in range(len(nums)):
-            nums[i]=(pre[i-1] if 0<i else 1)*(suf[i+1] if i<len(nums)-1 else 1)
-        return nums
+        pre,suf,n=list(accumulate(nums,mul)), list(accumulate(nums[::-1],mul))[::-1],len(nums)
+        return [(pre[i-1] if i else 1)*(suf[i+1] if i+1<n else 1) for i in range(n)]
         
         
