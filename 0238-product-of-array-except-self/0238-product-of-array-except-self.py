cfa76dad9e794c00a77830582005631b17e4c563
Yeseul, Kim <yesl00k624@gmail.com>
2023-07-18 16:21:33 +0900

    Time: 249 ms (38.14%), Space: 24 MB (46.53%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,11 +3,11 @@ from operator import mul
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n,suf=len(nums),1
+        n,pre,suf=len(nums),1,1
         res=[1]*n
-        for i in range(1,n):
-            res[i]=res[i-1]*nums[i-1]
-        for i in range(n-1,-1,-1):
-            res[i]*=suf
-            suf*=nums[i]
+        for i in range(n):
+            res[i]*=pre
+            pre*=nums[i]
+            res[-1-i]*=suf
+            suf*=nums[-1-i]
         return res
