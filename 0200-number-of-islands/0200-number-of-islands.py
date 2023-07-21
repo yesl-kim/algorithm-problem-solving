f32e03d41f3ef1f8376f14f265365c9067569bc3
Yeseul, Kim <yesl00k624@gmail.com>
2023-07-21 09:50:14 +0900

    Time: 307 ms (90.42%), Space: 18.5 MB (99.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,8 +3,6 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         q=deque()
-        dx=[-1,0,1,0]
-        dy=[0,1,0,-1]
         m,n,cnt=len(grid),len(grid[0]),0
         for i in range(m):
             for j in range(n):
@@ -14,8 +12,8 @@ class Solution:
                     q.append((i,j))
                     while q:
                         x,y=q.popleft()
-                        for d in range(4):
-                            xx,yy=x+dx[d],y+dy[d]
+                        for dx,dy in ((-1,0),(0,1),(1,0),(0,-1)):
+                            xx,yy=x+dx,y+dy
                             if 0<=xx<m and 0<=yy<n and grid[xx][yy]=="1":
                                 grid[xx][yy]='0'
                                 q.append((xx,yy))
