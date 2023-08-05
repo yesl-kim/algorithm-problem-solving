f1a2fce9ddc099ad4bf1239b7cfe6f8ba46055a9
yesl-kim <yesl00k624@gmail.com>
2023-08-05 15:16:01 +0900

    Refactor: 3sum (2)

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,38 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        n,hash,res=len(nums),{},[]
-        for i in range(n-1):
-            if i and nums[i]==nums[i-1]: continue
-            for j in range(i+1,n):
-                x=nums[j]
-                c=hash.get(x)
-                if c:
-                    res.append(c+[x])
-                    del hash[x]
-                elif i+1<j and nums[j-1]==x:
-                    continue
-                else:
-                    hash[-(nums[i]+nums[j])]=[nums[i],nums[j]]
-            hash.clear()
-        return res
-                
-                    
-                    
-                
+# mine
+def threeSum(nums):
+         nums.sort()
+         n,hash,res=len(nums),{},[]
+
+         for i in range(n-1):
+             if nums[i]==nums[i+1]: continue
+
+             for j in range(i+1,n):
+                 x=nums[j]
+                 c=hash.get(x)
+
+                 if c:
+                     res.append(c+[x])
+                     del hash[x]
+                 elif nums[j+1]==x:
+                     continue
+                 else:
+                     hash[-(nums[i]+nums[j])]=[nums[i],nums[j]]
+
+             hash.clear()
+         return res
+
+# print(threeSum([-1,0,1,2,-1,-4]))
+
+
+
+from itertools import combinations
+
+
+def threesums(xs):
+    return [(xs[i], xs[j], xs[k])
+            for (i, j, k) in combinations(range(len(xs)), 3)
+            if xs[i] + xs[j] + xs[k] == 0]
+
+
+print(threesums([-1,0,1,2,-1,-4]))
+
--- 0015-3sum/3sum(2).py
+++ 0015-3sum/3sum(2).py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from collections import defaultdict
 
 
 # 1) 조합 구하기
@@ -8,23 +9,48 @@ from itertools import combinations
 # 2) 조건에 맞는 조합 구하기
 
 def threesum(xs):
+
+    sums=defaultdict(list)
+    for i, v in enumerate(xs):
+        sums[v].append(i)
+
+    def has(i, j, z):
+        for k in sums[z]: # (1)
+            if k != i and k != j:
+                sums[z].remove(k) # (2)
+                return True
+        return False
+    
+    res = set()
+    for (i, j) in combinations(range(len(xs)), 2):
+        x, y, z = xs[i], xs[j], -(xs[i] + xs[j])
+        if has(i, j, z):
+            res.add(tuple(set([x,y,z]))) # (3)
+    
+    return list(res)
+
+def threesum(xs):
     res = []
+
+    def findAfter(i, j, z):
+        return z in set(xs[max(i, j) + 1: ])
     
     for (i, j) in combinations(range(len(xs)), 2):
         x, y, z = xs[i], xs[j], -(xs[i] + xs[j])
-        if z in set(xs[j + 1: ]):
+        if findAfter(i, j, z):
             res.append((x, y, z))
     
     return res
 
 
 # 첫 출발 (참고 코드)
-# def threesum(xs):
-#     return [(xs[i], xs[j], xs[k])
-#             for (i, j, k) in combinations(range(len(xs)), 3)
-#             if xs[i] + xs[j] + xs[k] == 0]
+def threesums(xs):
+    return [(xs[i], xs[j], xs[k])
+            for (i, j, k) in combinations(range(len(xs)), 3)
+            if xs[i] + xs[j] + xs[k] == 0]
 
 
-print(threesum([1, 2, -3, -4, -1]))
+print(threesum([1, 2, -3, -4, -1, 0]))
+print(threesums([1, 2, -3, -4, -1, 0]))
 
 # set -> -1, 0, 1, 2, -4
