ad23645d4d3675347e153b1029799c8eb45b13c3
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-08 08:10:30 +0900

    Time: 49 ms (57.33%), Space: 16.3 MB (69.78%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -3,11 +3,10 @@ from functools import reduce
 
 class Solution:
     def isHappy(self, n: int) -> bool:
-        memo = defaultdict(int)
+        memo = set()
         
-        while n != 1 and not memo[n]:
-            square = reduce(lambda acc, cur: acc + int(cur)**2, str(n), 0)
-            memo[n] = square
-            n = square
+        while n != 1 and n not in memo:
+            memo.add(n)
+            n = reduce(lambda acc, cur: acc + int(cur)**2, str(n), 0)
         
         return n == 1
