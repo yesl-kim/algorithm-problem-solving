6ade2c21fe4a6ecdcbf1f0385a378d9e18272c48
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-07 10:44:54 +0900

    Time: 43 ms (98.54%), Space: 16.8 MB (64.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,18 +5,13 @@ class Solution:
         if len(s) != len(t):
             return False
         
-        counts = defaultdict(int)
-        for x in s:
-            counts[x] += 1
+        count = defaultdict(int)
+        for char in s:
+            count[char] += 1
             
-        for x in t:
-            if not counts[x]:
-                return False
-            
-            counts[x] -= 1
+        for char in t:
+            if not count[char]:
+                return False 
+            count[char] -= 1
         
-        for n in counts.values():
-            if n > 0:
-                return False
-        else:
-            return True
+        return True
