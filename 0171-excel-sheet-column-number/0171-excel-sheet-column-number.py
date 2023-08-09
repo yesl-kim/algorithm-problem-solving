9d7eac24339ffbe4bae76a49063240708829a568
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-09 20:42:30 +0900

    Time: 43 ms (78.22%), Space: 16.3 MB (67.95%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,12 @@
+get_number = lambda word: ord(word) - 64
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+        decimals = 26
+        cnt = len(columnTitle) - 1
+        for word in columnTitle:
+            res += (decimals**cnt) * get_number(word)
+            cnt -= 1
+        
+        return res
