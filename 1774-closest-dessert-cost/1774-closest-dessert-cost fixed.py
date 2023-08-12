8a4c7671314ec902379c316f7d150ce71503e987
yesl-kim <yesl00k624@gmail.com>
2023-08-12 17:27:58 +0900

    Refactor: (1774) closest dessert cost (2)
    - nonlocal

--- 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
@@ -1,26 +1,57 @@
+from functools import reduce
+
 def closestCost(baseCosts, toppingCosts, target):
-    res=float('inf')
-        
-    def make(i, total, acc):
-        print(i, total, acc)
-
+    
+    def calculate_closest(i, total):
+        nonlocal res
         if i == len(toppingCosts):
-            abs_cur, abs_acc = abs(target - total), abs(target - acc)
-            if abs_cur < abs_acc:
+            abs_cur, abs_res = abs(target-total), abs(target-res)
+            if abs_cur < abs_res:
                 return total
-            elif abs_cur > abs_acc:
-                return acc
+            elif abs_res < abs_cur:
+                return res
             else:
-                return min(total, acc)
+                return min(res, total)
         else:
-            for count in range(3):
-                acc = make(i+1, total + (count * toppingCosts[i]), acc)
-            return acc
+            for count in range(0, 3):
+                res = calculate_closest(i+1, total + toppingCosts[i] * count)
+            return res
 
+    res=float('inf')
     for bc in baseCosts:
-        res = make(0, bc, res)
+        res = calculate_closest(0, bc)
         
     return res
+
+
+def closestCost_demo(baseCosts, toppingCosts, target):
+
+    def find_closer(a, b):
+            abs_a, abs_b = abs(target-a), abs(target-b)
+            if abs_a == abs_b:
+                return min(a, b)
+            else:
+                val = {
+                    abs_a: a,
+                    abs_b: b
+                }
+                return val[min(abs_a, abs_b)]
+    
+    def calculate_cost(i, total):
+        if i == len(toppingCosts):
+            return total
+        else:
+            costs = {calculate_cost(i+1, total + toppingCosts[i] * num) for num in range(3)}
+            return reduce(find_closer, costs, res)
+
+    res=float('inf')
+    for bc in baseCosts:
+        res = find_closer(res, calculate_cost(0, bc))
+        
+    return res
+
+
+
 
 
 baseCost = [1,7]
