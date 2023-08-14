5fe4c310091a5b4a60283d2913523ea0909990bb
yesl-kim <yesl00k624@gmail.com>
2023-08-14 10:19:29 +0900

    Refactor: (1774) closest dessert cost (3)
    - closestCost_demo
    - res를 집합 자료형으로 변경

--- 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
@@ -3,7 +3,7 @@ from functools import reduce
 def closestCost(baseCosts, toppingCosts, target):
     
     def calculate_closest(i, total):
-        nonlocal res
+        # nonlocal res # <-- 얘를 빼
         if i == len(toppingCosts):
             abs_cur, abs_res = abs(target-total), abs(target-res)
             if abs_cur < abs_res:
@@ -24,6 +24,42 @@ def closestCost(baseCosts, toppingCosts, target):
     return res
 
 
+def closestCost(baseCosts, toppingCosts, target):
+
+    def find_closer(a, b):
+        abs_a, abs_b = abs(target-a), abs(target-b)
+        if abs_a < abs_b:
+            return a
+        elif abs_b < abs_a:
+            return b
+        else:
+            return min(a, b)
+    
+    def calculate_closest(i, total):
+        if i == len(toppingCosts):
+            return total
+        
+        cost = total
+        for count in range(1, 3):
+            next_cost = calculate_closest(i+1, total + (count * toppingCosts[i]))
+            abs_cur, abs_next = abs(target-cost), abs(target-next_cost)
+            if abs_cur == abs_next:
+                return min(cost, next_cost)
+            else:
+                val = {
+                    abs_cur: cost,
+                    abs_next: next_cost
+                }
+                return val[min(abs_cur, abs_next)]
+        return cost
+
+    res=float('inf')
+    for bc in baseCosts:
+        res = find_closer(res, calculate_closest(0, bc))
+        
+    return res
+
+
 def closestCost_demo(baseCosts, toppingCosts, target):
 
     def find_closer(a, b):
@@ -39,22 +75,26 @@ def closestCost_demo(baseCosts, toppingCosts, target):
     
     def calculate_cost(i, total):
         if i == len(toppingCosts):
-            return total
-        else:
-            costs = {calculate_cost(i+1, total + toppingCosts[i] * num) for num in range(3)}
-            return reduce(find_closer, costs, res)
-
-    res=float('inf')
-    for bc in baseCosts:
-        res = find_closer(res, calculate_cost(0, bc))
+            res.add(total)
+            return
         
-    return res
+        for count in range(3):
+            calculate_cost(i+1, total + toppingCosts[i] * count)
+
+    res=set()
+    for bc in baseCosts:
+        calculate_cost(0, bc)
+
+    return reduce(find_closer, res, float('inf'))
 
 
 
 
 
-baseCost = [1,7]
-toppingCosts = [3,4]
-target = 10
-print(closestCost(baseCost, toppingCosts, target))
+# baseCost = [1,7]
+# toppingCosts = [3,4]
+# target = 10
+baseCost = [2, 3]
+toppingCosts = [4, 5, 100]
+target = 18
+print(closestCost_demo(baseCost, toppingCosts, target))
