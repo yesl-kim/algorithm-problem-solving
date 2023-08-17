a73e5e2d035ba8c24e86c9afd87084657fe07969
yesl-kim <yesl00k624@gmail.com>
2023-08-17 15:13:57 +0900

    Refactor: closest dessert cost (4)
    - 노트 정리 추가
    - calculate_closest의 toppingCosts 참조 제거 -> every_costs
    - product 활용

--- 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost fixed.py
@@ -1,32 +1,9 @@
 from functools import reduce
-
-def closestCost(baseCosts, toppingCosts, target):
-    
-    def calculate_closest(i, total):
-        # nonlocal res # <-- 얘를 빼
-        if i == len(toppingCosts):
-            abs_cur, abs_res = abs(target-total), abs(target-res)
-            if abs_cur < abs_res:
-                return total
-            elif abs_res < abs_cur:
-                return res
-            else:
-                return min(res, total)
-        else:
-            for count in range(0, 3):
-                res = calculate_closest(i+1, total + toppingCosts[i] * count)
-            return res
-
-    res=float('inf')
-    for bc in baseCosts:
-        res = calculate_closest(0, bc)
-        
-    return res
-
+from itertools import product
 
 def closestCost(baseCosts, toppingCosts, target):
 
-    def find_closer(a, b):
+    def closer(a, b):
         abs_a, abs_b = abs(target-a), abs(target-b)
         if abs_a < abs_b:
             return a
@@ -35,34 +12,27 @@ def closestCost(baseCosts, toppingCosts, target):
         else:
             return min(a, b)
     
-    def calculate_closest(i, total):
+    def calculate_cost(i, total):
         if i == len(toppingCosts):
             return total
         
         cost = total
         for count in range(1, 3):
-            next_cost = calculate_closest(i+1, total + (count * toppingCosts[i]))
-            abs_cur, abs_next = abs(target-cost), abs(target-next_cost)
-            if abs_cur == abs_next:
-                return min(cost, next_cost)
-            else:
-                val = {
-                    abs_cur: cost,
-                    abs_next: next_cost
-                }
-                return val[min(abs_cur, abs_next)]
+            cost = closer(cost, calculate_cost(i+1, total + toppingCosts[i] * count))
         return cost
 
     res=float('inf')
     for bc in baseCosts:
-        res = find_closer(res, calculate_closest(0, bc))
-        
+        res = closer(res, calculate_cost(0, bc))
+    
     return res
 
 
-def closestCost_demo(baseCosts, toppingCosts, target):
+# 1. calculate_closest의 toppingCosts 참조 제거 -> every_costs
+# 2. product 활용
+def closestCost2(baseCosts, toppingCosts, target):
 
-    def find_closer(a, b):
+    def closer(a, b):
             abs_a, abs_b = abs(target-a), abs(target-b)
             if abs_a == abs_b:
                 return min(a, b)
@@ -72,29 +42,42 @@ def closestCost_demo(baseCosts, toppingCosts, target):
                     abs_b: b
                 }
                 return val[min(abs_a, abs_b)]
-    
-    def calculate_cost(i, total):
-        if i == len(toppingCosts):
-            res.add(total)
-            return
+
+    # def calculate_closest(total, costs):
+    #     if not costs:
+    #         return total
         
-        for count in range(3):
-            calculate_cost(i+1, total + toppingCosts[i] * count)
+    #     costs = [calculate_closest(total + count * costs[0], costs[1:]) for count in range(3)]
+    #     print(costs)
+    #     return costs
+    
+    def every_costs(total, topping_costs):
+        return [reduce(calculate, enumerate(cb), total) for cb in product(range(3), repeat=len(topping_costs))]
+            
+        # if not costs:
+        #     return [total]
+        
+        # costss = [every_costs(total + count * costs[0], costs[1:]) for count in range(3)] # 3중 for문
+        # return costss[0] + costss[1] + costss[2]
+    
+    def calculate(total, topping):
+        idx, count = topping
+        return total + toppingCosts[idx] * count
 
-    res=set()
+
+    res = float('inf')
     for bc in baseCosts:
-        calculate_cost(0, bc)
-
-    return reduce(find_closer, res, float('inf'))
-
+        res = every_costs(bc, toppingCosts)
+        print(res)
 
 
 
 
-# baseCost = [1,7]
-# toppingCosts = [3,4]
-# target = 10
-baseCost = [2, 3]
-toppingCosts = [4, 5, 100]
-target = 18
-print(closestCost_demo(baseCost, toppingCosts, target))
+
+baseCost = [1,7]
+toppingCosts = [3,4]
+target = 10
+# baseCost = [2, 3]
+# toppingCosts = [4, 5, 100]
+# target = 18
+print(closestCost2(baseCost, toppingCosts, target))
--- 1774-closest-dessert-cost/NOTES.md
+++ 1774-closest-dessert-cost/NOTES.md
@@ -1,7 +1,29 @@
-​global과 nonlocal
+피드백
 
-- global: 전역변수 -> 전역에서 해당 변수를 찾음
-- nonlocal: 가장 가까운 상위 스코프에서 해당 변수를 찾음
-- global과 nonlocal을 쓰게 되면 외부 변수를 변경한다는 것이므로 좋지 않음 (영향 관계를 파악하기 어려움 -> 유지보수 어려움)
-- 반대로 외부에 영향을 주지 않도록 함수 내부에서 지역변수만을 사용 -> 계산 -> 값을 반환하는 형태가 제일 좋음 (순수함수)
-- https://stackoverflow.com/questions/71373859/sharing-variables-across-recursion-calls-lists-sets
+1.  **이번 문제에서는 global, nonlocal을 쓰지 않고 global, nonlocal 사용은 좋지 않다. 왜 그럴까?**
+
+    - global: 전역변수 -> 전역에서 해당 변수를 찾음
+    - nonlocal: 가장 가까운 상위 스코프에서 해당 변수를 찾음
+    - global과 nonlocal을 쓰게 되면 외부 변수를 변경한다는 것이므로 좋지 않음 (영향 관계를 파악하기 어려움 -> 유지보수 어려움)
+    - 반대로 외부에 영향을 주지 않도록 함수 내부에서 지역변수만을 사용 -> 계산 -> 값을 반환하는 형태가 제일 좋음 (순수함수)
+
+2.  순수함수의 이름은 형용사(?)로 짓는 것이 자연스럽다 (ex. min, closer, ...)
+
+3.  **처음 calculate 함수는 완전히 분리되지 않았다**
+
+        - 조합하는 함수 calculate 안에서 비교하여 하나의 값을 리턴하도록 closer 함수를 호출하고 있기 때문에
+        - calculate 안에서 조합과 비교를 같이 한다.
+
+    **`solution`**: calculate에서는 조합만, closer는 비교만
+    => calculate 함수에서는 조합한 금액의 총액의 리스트를 반환한다
+
+    **`problem`**: 이럴 경우 메모리를 너무 크게 차지 (공간 복잡도가 o(3^n))
+    => 제너레이터 활용!
+    => 제너레이터가 어렵다면, 제너레이터를 반환하는 내장 함수를 활용하는 것도 방법 (`product`)
+
+---
+
+참고
+
+- [global, nonlocal이 안좋은가](https://stackoverflow.com/questions/71373859/sharing-variables-across-recursion-calls-lists-setss)
+- [product 참고](https://stackstackstack.tistory.com/entry/python-순열-조합-함수-product-permutations-combinations)
