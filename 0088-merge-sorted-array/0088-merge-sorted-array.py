1eb3c0c65b1294c3f6dcba2b84fffcebc479523d
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-17 11:10:26 +0900

    Time: 35 ms (97.90%), Space: 16.2 MB (98.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # 공간 재배치
-        for i in range(m-1,-1,-1):
-            nums1[n+i] = nums1[i]
+        p1, p2 = m-1, n-1
+        i = m + n - 1
         
-        i = 0
-        p1, p2 = n, 0
-        while p1 < m + n and p2 < n:
+        while 0 <= p1 and 0 <= p2:
             if nums1[p1] < nums2[p2]:
-                nums1[i] = nums1[p1]
-                p1 += 1
-            else:
                 nums1[i] = nums2[p2]
-                p2 += 1
-            i += 1
+                p2-=1
+            else:
+                nums1[i] = nums1[p1]
+                p1-=1
+            i-=1
         
-        if p2 < n:
-            for j in range(p2, n):
-                nums1[i] = nums2[j]
-                i += 1
+        if 0 <= p2:
+            for i in range(p2+1):
+                nums1[i] = nums2[i]
+                
