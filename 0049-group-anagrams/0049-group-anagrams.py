20539eee51674bb7dae07bce8738651759823a4a
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-24 16:32:13 +0900

    Time: 81 ms (99.72%), Space: 20.3 MB (50.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,10 @@
 from collections import defaultdict, Counter
 
-key = lambda s: str(hash(''.join(sorted(s))))
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
         
         for s in strs:
-            anagrams[key(s)].append(s)
+            anagrams[''.join(sorted(s))].append(s)
                 
         return anagrams.values()
