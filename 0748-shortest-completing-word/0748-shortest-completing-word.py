3b92cc03ed53ca934a3b212853d8525880b9d28e
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-30 10:06:26 +0900

    Time: 103 ms (38.78%), Space: 16.6 MB (69.94%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -0,0 +1,14 @@
+import re
+from collections import Counter
+
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        res = max(words, key=len) + 'xxx'
+        origin = Counter(re.sub('[\d\s]', '', licensePlate).lower())
+        
+        for word in words:
+            occurrences = Counter(word)
+            if origin <= occurrences:
+                res = min(res, word, key=len)
+                
+        return res
