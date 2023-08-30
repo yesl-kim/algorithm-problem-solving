9c914873934b84af9c469a495e33e36482dc3596
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-30 09:42:37 +0900

    Time: 591 ms (17.55%), Space: 16.7 MB (81.48%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        common = defaultdict(list)
+        
+        for i, x in enumerate(list1):
+            for j, y in enumerate(list2):
+                if x == y:
+                    common[i+j].append(x)
+        
+        return common[sorted(common)[0]]
