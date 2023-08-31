75bcf39bb63b352addb98c94e1edec974bb66003
Yeseul, Kim <yesl00k624@gmail.com>
2023-08-31 10:38:04 +0900

    Time: 44 ms (54.41%), Space: 16.2 MB (93.18%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def move(self, nums, left, right, val):
+        while left < len(nums) and nums[left] != val:
+            left += 1
+        
+        while 0 <= right and nums[right] == val:
+            right -= 1
+        
+        return left, right
+    
+    
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = self.move(nums, 0, len(nums) - 1, val)
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left, right = self.move(nums, 0, len(nums) - 1, val)
+        
+        return left
