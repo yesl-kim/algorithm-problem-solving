68e6d83cf11d04f16fe27dc44f36d634b8c9d06b
yesl-kim <yesl00k624@gmail.com>
2023-09-05 10:48:38 +0900

    Refactor: generate parentheses

---
+++ 0022-generate-parentheses/0022-generate-parentheses.modified.py
@@ -0,0 +1,29 @@
+from typing import List
+from functools import reduce
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opens=[]
+
+        for x in s:
+            if x == '(':
+                opens.append(x)
+            else:
+                if not opens:
+                    return False
+                opens.pop()
+
+        return len(opens) == 0
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        pairs = ('((', '()', '))', ')(')
+        def generate(n, s = ''):
+            if n == 0:
+                return [s] if self.isValid(s) else []
+            else:
+                return reduce(lambda acc, p: acc + generate(n-1, s + p), pairs, [])
+
+        return generate(n)
+
+s = Solution()
+print(s.generateParenthesis(3))
