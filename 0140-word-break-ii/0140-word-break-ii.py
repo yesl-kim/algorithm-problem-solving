401abe68a4ac8ca64d8b570056072aa9195d2963
Yeseul, Kim <yesl00k624@gmail.com>
2023-09-12 16:05:20 +0900

    Time: 32 ms (91.91%), Space: 16.4 MB (55.18%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        def dfs(s, sentence = ''):
+            if not s:
+                res.append(sentence.strip())
+                return
+            
+            for w in wordDict:
+                if s.startswith(w):
+                    dfs(s[len(w):], sentence + ' ' + w)
+            
+        dfs(s)
+        return res
+            
