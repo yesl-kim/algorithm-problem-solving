f6280f9573376850b394d7c309a19d65683876c4
Yeseul, Kim <yesl00k624@gmail.com>
2023-10-01 11:00:22 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,23 @@
+
+
+def solution(citations):
+    citations.sort()
+    def count(v):
+        for i, x in enumerate(citations):
+            if v <= x:
+                return len(citations) - i
+                
+    
+    l, r = 0, citations[-1]
+    res = 0
+    while l<= r:
+        h = (l+r) //2
+        if count(h) < h:
+            r = h - 1
+        else:
+            res = h
+            l = h + 1
+    return res
+    
+    
+            
