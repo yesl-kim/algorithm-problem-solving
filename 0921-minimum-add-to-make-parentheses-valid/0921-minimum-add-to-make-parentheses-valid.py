95459c6cef0b9f6a96558212eb5fd646d6b08bec
Yeseul, Kim <yesl00k624@gmail.com>
2023-10-04 13:57:07 +0900

    Time: 47 ms (6.58%), Space: 16.3 MB (14.87%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        opens, closes = 0, 0
+        for p in s:
+            if p == '(':
+                opens += 1
+            else:
+                if 0 < opens:
+                    opens -= 1
+                else:
+                    closes += 1
+        return opens + closes
