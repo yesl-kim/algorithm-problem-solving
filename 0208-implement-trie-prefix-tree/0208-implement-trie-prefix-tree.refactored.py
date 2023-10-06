11c0aa5fd73614b58ac49ab671865d3f381b399a
yesl-kim <yesl00k624@gmail.com>
2023-10-06 11:15:05 +0900

    Refactor: implement trie prefix tree
    - 반복되는 로직 분리

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.refactored.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self):
+        self.next = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+
+    def find(self, s: str) -> Node:
+        node = self.root
+        for char in s:
+            if not char in node.next:
+                raise Exception(node, char)
+            node = node.next[char]
+        return node
+    
+
+    def insert(self, word: str) -> None:
+        while True:
+            try:
+                self.find(word).isEnd = True
+                return
+            except Exception as error:
+                node, char = error.args
+                node.next[char] = Node()
+
+        
+    def search(self, word: str) -> bool:
+        try:
+            return self.find(word).isEnd
+        except:
+            return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        try:
+            self.find(prefix)
+            return True
+        except:
+            return False
--- 0208-implement-trie-prefix-tree/NOTES.md
+++ 0208-implement-trie-prefix-tree/NOTES.md
@@ -1,1 +1,21 @@
-​
+## 리팩토링
+
+### 1. 반복되는 로직 분리
+
+- 주어진 word가 트리에 있는지 확인하는 로직
+- 예외 처리를 통해 word가 트리에 있는지 없는지 구분할 수 있음
+
+알게된 것
+
+- 예외처리시 값 throw 하기
+
+  ```py
+  ...
+  raise Exception(a, b, ...) # 여러 값을 throw 할 수 있음
+
+  ...
+  try:
+    ...
+  except Exception as error:
+    a, b = error.args # 값은 args 프로퍼티에 담기고, 이를 unpack할 수 있음
+  ```
