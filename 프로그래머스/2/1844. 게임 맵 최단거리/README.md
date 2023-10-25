53bce0afa81d2c004b75b0d49665d6c19c443804
Yeseul, Kim <yesl00k624@gmail.com>
2023-10-25 16:01:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 7.71 ms
+메모리: 10.3 MB, 시간: 5.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 15:55:7
+2023년 10월 3일 16:1:35
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -4,13 +4,20 @@ def solution(maps):
     q = deque()
     h, w = len(maps), len(maps[0])
     q.append((0,0))
+
+    def path(x, y):
+        for dx, dy in ((0,1), (1,0), (0,-1), (-1,0)):
+            xx, yy = x+dx, y+dy
+            if 0 <= xx < h and 0 <= yy < w and maps[xx][yy] == 1:
+                yield xx, yy
+
     while q:
         i, j = q.popleft()
-        for dx, dy in ((0,1), (1,0), (0,-1), (-1,0)):
-            x, y = i+dx, j+dy
-            if 0 <= x < h and 0 <= y < w and maps[x][y] == 1:
-                maps[x][y] = maps[i][j] + 1
-                q.append((x, y))
-                
-    path = maps[h-1][w-1]
-    return path if path > 1 else -1
+        if i == h-1 and j == w-1:
+            return maps[i][j]
+        
+        for x, y in path(i, j):
+            maps[x][y] = maps[i][j] + 1
+            q.append((x, y))
+
+    return -1
