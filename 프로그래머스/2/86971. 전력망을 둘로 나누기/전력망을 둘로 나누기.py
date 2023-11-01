dff8d8af44ccb2f20a59a548e058db90ef899eb5
Yeseul, Kim <yesl00k624@gmail.com>
2023-11-01 11:49:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 51.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,30 +1,44 @@
+class Union_Find:
+    def __init__(self, N):
+        self.root = list(range(N))
+        self.height = [1]*N
+
+    def find(self, x):
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def connect(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return
+        
+        hx, hy = self.height[rx], self.height[ry]
+        if hx < hy:
+            self.root[rx] = ry
+        else:
+            self.root[ry] = rx
+            
+            if hx == hy:
+                self.height[rx] += 1
+
+    def count(self, x):
+        root = self.find(x)
+        def count_children(parent):
+            return sum(1 + count_children(c) for c, p in enumerate(self.root) if c != parent and p == parent)
+        return count_children(root) + 1
+    
+    
 def solution(n, wires):
-    maps = [[False] * (n + 1) for _ in range(n + 1)] # 1 indexed
-    for x, y in wires:
-        maps[x][y] = True
-        maps[y][x] = True
-    
-    def count(x):
-        res = 1
-        for y, connected in enumerate(maps[x]):
-            if not connected:
-                continue
-            maps[x][y] = False
-            maps[y][x] = False
-            res += count(y)
-            maps[x][y] = True
-            maps[y][x] = True
-        return res
-        # return 1 + sum(count(y) for y, connected in enumerate(maps[x]) if connected)
-    
     res = float('inf')
-    for x, y in wires:
-        maps[x][y] = False
-        maps[y][x] = False
-        res = min(res, abs(count(x) - count(y)))
-        maps[x][y] = True
-        maps[y][x] = True
-    
+    for i in range(len(wires)):
+        tree = Union_Find(n + 1) # 1 indexed, 1 ~ n
+        new_wires = wires[:i] + wires[i+1:]
+        for t1, t2 in new_wires:
+            tree.connect(t1, t2)
+        
+        x, y = wires[i]
+        res = min(res, abs(tree.count(x) - tree.count(y)))
     return res
-        
-        
+    
