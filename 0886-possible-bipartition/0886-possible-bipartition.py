22fdd66ab774ef417111cf48a54b4a0b4f5f3f00
Yeseul, Kim <yesl00k624@gmail.com>
2023-11-11 14:46:55 +0900

    Time: 681 ms (16.36%), Space: 30 MB (5.04%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -5,18 +5,16 @@ class Solution:
             dislikes_map[a].add(b)
             dislikes_map[b].add(a)
         
-        groups = defaultdict(set) # -1, 1
-        group_num = defaultdict(int)
+        group = defaultdict(int) # -1, 0, 1
         def can_put_person(person, i):
-            g = group_num[person]
+            g = group[person]
             if not g:
-                group_num[person] = i
-                groups[i].add(person)
+                group[person] = i
                 return all(can_put_person(dislike, -i) for dislike in dislikes_map[person])
             return g == i
         
         for person in range(1, n + 1):
-            if not group_num[person]:
+            if not group[person]:
                 if not can_put_person(person, 1):
                     return False
         return True
