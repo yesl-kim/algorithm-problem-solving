92b23b7681c8bdb8e74be5fd6049b151345e19d8
Yeseul, Kim <yesl00k624@gmail.com>
2023-11-22 16:00:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 1.63 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -7,6 +7,6 @@ def solution(n):
         if n <= 0:
             return 0
         if not memo[n]:
-            memo[n] = count(n-1) + count(n-2)
+            memo[n] = (count(n-1) + count(n-2)) % 1234567
         return memo[n]
     return count(n) % 1234567
