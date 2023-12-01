ad4aa987876e1ca14983aba5315c79a8fe674d7a
Yeseul, Kim <yesl00k624@gmail.com>
2023-12-01 17:58:01 +0900

    Time: 354 ms (40.37%), Space: 22.4 MB (43.94%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.py
@@ -2,15 +2,13 @@ import math
 
 class Solution:
     def smallestDivisor(self, nums: List[int], threshold: int) -> int:
-        def check(divisor):
-            return sum(math.ceil(n / divisor) for n in nums) <= threshold
-        
         lt, rt = 1, max(nums)
         res = None
+        check = lambda divisor: sum(math.ceil(n / divisor) for n in nums) <= threshold
+        
         while lt <= rt:
             mid = (lt + rt) // 2
             if check(mid):
-                # 최솟값을 찾음
                 res = mid
                 rt = mid - 1
             else:
