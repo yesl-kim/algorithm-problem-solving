84eec7d5949eb4a0d81daba0bfcd1e86e2e01679
Yeseul, Kim <yesl00k624@gmail.com>
2023-12-02 12:10:28 +0900

    Time: 50 ms (6.32%), Space: 17.2 MB (8.43%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(2 * (10**9))
+
+def memoize(fn):
+    cache = {}
+    def memoizer(*args, **kargs):
+        key = str(args) + str(kargs)
+        if not key in cache:
+            cache[key] = fn(*args, **kargs)
+        return cache[key]
+    return memoizer
+        
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @memoize
+        def paths(x, y):
+            if not (0 <= x < m) or not (0 <= y < n):
+                return 0
+            
+            if x == m - 1 and y == n - 1:
+                return 1
+            
+            return paths(x, y + 1) + paths(x + 1, y)
+        return paths(0, 0)
