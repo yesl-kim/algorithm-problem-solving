9ca04e694b12f235b9c1e6e417a943fb311c49a3
Yeseul, Kim <yesl00k624@gmail.com>
2023-12-21 11:19:59 +0900

    [level 2] Title: [3차] 압축, Time: 1.00 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 21.70 ms
+메모리: 10.5 MB, 시간: 1.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 11:7:54
+2023년 12월 4일 11:19:57
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,22 +1,21 @@
 def solution(msg):
     dic = {chr(i + 64): i for i in range(1, 27)}
     def find_prefix(s):
-        for i in range(len(s), 0, -1):
+        if s in dic:
+            return s
+        for i in range(1, len(s) + 1):
             prefix = s[:i]
-            if prefix in dic:
-                return prefix
+            if prefix not in dic:
+                return prefix[:-1]
     
-    next_index = 27
     def zzip(s):
-        nonlocal next_index
         prefix = find_prefix(s)
         index = dic[prefix]
         s = s[len(prefix):]
         if not s:
             return [index]
         
-        dic[prefix + s[0]] = next_index
-        next_index += 1
+        dic[prefix + s[0]] = len(dic) + 1
         return [index] + zzip(s)
     
     return zzip(msg)
