0f9b72806db71fece745a08c3f0213508986ca5b
Yeseul, Kim <yesl00k624@gmail.com>
2023-12-21 12:25:14 +0900

    Time: 44 ms (21.80%), Space: 17.5 MB (20.25%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,34 +1,31 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        # 총 길이 구하는 건,, 함수 분리 -> 그때그때 구하는게 좋을까
         padEnd = lambda s: s + ' ' * (maxWidth - len(s))
+        get_width = lambda words: len(words) - 1 + sum(len(w) for w in words)
             
-        def pack(words, width):
+        def pack(words):
             words_num = len(words)
             if words_num == 1:
                 return padEnd(words[0])
             
-            share, rest = divmod(maxWidth - width, words_num - 1)
-            space = ' ' * (share + 1)
-            
+            share, rest = divmod(maxWidth - get_width(words), words_num - 1)
+            default_space = ' ' * (share + 1)
             line = words[0]
             for i, word in enumerate(words[1:]):
-                line += space + ' ' + word if i < rest else space + word
+                space = default_space + ' ' if i < rest else default_space
+                line += space + word
             return line
                 
                 
         line = [words[0]]
-        width = len(words[0])
         output = []
         for word in words[1:]:
-            new_width = width + len(word) + 1
-            if new_width <= maxWidth:
-                width = new_width
+            width = get_width(line) + len(word) + 1
+            if width <= maxWidth:
                 line.append(word)
             else:
-                output.append(pack(line, width))
+                output.append(pack(line))
                 line = [word]
-                width = len(word)
                 
         if line:
             last_line = padEnd(" ".join(line))
