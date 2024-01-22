b5db36df87b27e134e5089e8c52d026e66a30e1d
yesl-kim <yesl00k624@gmail.com>
2024-01-22 13:07:06 +0900

    NOTES: 파일명 정렬

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -4,7 +4,7 @@ def solution(files):
     pattern = re.compile('(\D+)(\d{1,5})(.*)')
     
     def sort_fn(s):
-        head, number, tail = pattern.match(s).groups()
+        head, number, _ = pattern.match(s).groups()
         return head.lower(), int(number)
     
     return sorted(files, key=sort_fn)
