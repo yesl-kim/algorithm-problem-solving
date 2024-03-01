78c4002cc05859d9f506b9d0078f6fd99e700eb0
Yeseul, Kim <yesl00k624@gmail.com>
2024-03-01 10:28:59 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,29 +1,39 @@
-
-def solution(m, musicinfos):    
-    notes = {'C#': 'c', 'D#': 'd', "F#": 'f', "G#": 'g', 'A#': 'a'}
+def solution(m, musicinfos):
+    notes = {'C#': 'c', 'D#': 'd', 'F#': 'f', 'G#': 'g', 'A#': 'a', 'B#': 'b'}
+    def convert_music(s):
+        converted = s
+        for x, new in notes.items():
+            converted = converted.replace(x, new)
+        return converted
     
-    def parse_music(m):
-        parsed = m
-        for note, s in notes.items():
-            parsed = parsed.replace(note, s)
-        return parsed
-
-    def to_minutes(time):
-        hour, minuite = map(int, time.split(':'))
-        return hour * 60 + minuite
+    def get_duration(time):
+        h, m = map(int, time.split(':'))
+        return h * 60 + m
     
-    musics = []
-    melody = parse_music(m)
-    for info in musicinfos:
-        start, end, title, music = info.split(',')
-        duration = to_minutes(end) - to_minutes(start)
-
-        parsed_music = parse_music(music)
-        몫, 나머지 = divmod(duration, len(parsed_music))
-        played = parsed_music * 몫 + parsed_music[:나머지]
-
-        if melody in played:
-            musics.append((played, title))
+    def is_matched(musicinfo):
+        start, end, title, music = x.split(',')
+        music = converted(music)
+        
+        played_time = get_duration(end) - get_duration(start)
+        c, rest = divmod(played_time, len(music))
+        
+        played = music * c + music[:rest]
+        return m in played
     
-    musics.sort(key=lambda x: len(x[0]), reverse=True)
-    return musics[0][1] if musics else "(None)"
+    m = convert_music(m)
+    listened = []
+    for x in musicinfos:
+        start, end, title, music = x.split(',')
+        music = convert_music(music)
+        
+        played_time = get_duration(end) - get_duration(start)
+        c, rest = divmod(played_time, len(music))
+        
+        played = music * c + music[:rest]
+        if m in played:
+            listened.append((played, title))
+    
+    listened.sort(key=lambda x: len(x[0]), reverse=True)
+    return listened[0][1] if listened else '(None)'
+    
+        
