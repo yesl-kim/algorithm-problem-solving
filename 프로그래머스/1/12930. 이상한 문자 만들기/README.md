363bd3d9c85d46860450d0243087127514bcbb95
Yeseul, Kim <yesl00k624@gmail.com>
2024-04-28 08:10:35 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 08:06:24
+2024년 04월 28일 08:10:33
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,15 +1,7 @@
 def solution(s):
-    i = 0
-    ns = ''
-    for char in s:
-        if char == ' ':
-            i = 0
-            ns += char
-            continue
-        
-        if i % 2 == 0:
-            ns += char.upper()
-        else:
-            ns += char.lower()
-        i += 1
-    return ns
+    convert = lambda s: "".join(
+        char.upper() if i % 2 == 0 else char.lower() 
+        for i, char in enumerate(s)
+    )
+    
+    return " ".join(convert(s) for s in s.split(" "))
