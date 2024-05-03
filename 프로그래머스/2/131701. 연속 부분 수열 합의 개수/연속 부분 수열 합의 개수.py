ab50b01feb197ec2b295e2714869186b4462c66e
Yeseul, Kim <yesl00k624@gmail.com>
2024-05-03 13:06:58 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5003.70 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,12 @@
+def circular_window(arr, n):
+    pass
+
+def solution(elements):
+    expanded = elements * 2
+    res = set()
+    for start in range(len(elements)):
+        for window in range(1, len(elements)):
+            res.add(sum(expanded[start:start+window]))
+    res.add(sum(elements))
+    return len(res)
+    
