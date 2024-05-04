253a2a03fb0b9634969fcb0c933983dfa44621bb
Yeseul, Kim <yesl00k624@gmail.com>
2024-05-05 08:41:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 04일 18:01:35
+2024년 05월 05일 08:41:15
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -4,17 +4,15 @@ def is_valid_parenthesis(s):
     for x in s:
         if x in pairs:
             opens.append(x)
-        elif not opens or x is not pairs[opens.pop()]:
+        elif not (opens and x == pairs[opens.pop()]):
             return False
     
     return len(opens) == 0
 
 def solution(s):
     cnt = 0
-    i = 0
-    while i < len(s):
+    for i in range(len(s)):
         ns = s[i:] + s[0:i]
         if is_valid_parenthesis(ns):
             cnt += 1
-        i += 1
     return cnt
