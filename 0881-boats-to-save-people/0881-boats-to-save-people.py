fab1bdb30cb95b6dc9fbd8d40f069daa6b3f4516
Yeseul, Kim <yesl00k624@gmail.com>
2024-05-04 17:39:59 +0900

    Time: 356 ms (34.48%), Space: 23.4 MB (25.52%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,12 +1,13 @@
-from collections import deque
-
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
         cnt = 0
-        people = deque(sorted(people))
-        while people:
-            x = people.pop()
-            if people and people[0] + x <= limit:
-                people.popleft()
+        
+        while left < right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
             cnt += 1
-        return cnt
+        
+        return cnt + (left == right)
